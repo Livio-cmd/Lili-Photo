eb4257982b334f2104730171ad95db9905d39a0a
Livio <r91.misfit13@gmail.com>
2022-12-26 18:21:42 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+let mybutton = document.getElementById("scroll-back");
+const buttonMenu = document.getElementById("menu-btn");
+const menu = document.querySelector('nav > ul');
+const menuLinks = document.querySelectorAll("nav > ul li a");
+let open = false;
+const main = document.querySelector("main");
+const phone = document.getElementById('phone')
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+const copyPhone = () => {
+  navigator.clipboard.writeText("06 14 75 15 04");
+  //alert("Numéro de téléphone copié !");
+  phone.innerText = "copié !";
+  phone.classList.toggle('active');
+  setTimeout(()=>{
+    document.getElementById('phone').innerText = "06 14 75 15 04";
+    phone.classList.toggle('active');
+  }, 2000);
+}
+
+const resetNav = () => {
+  menu.style.transform = "translateX(0)";
+}
+
+
+menuLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    if(window.innerWidth < 1920){
+      toggleMenu();
+    }
+  });
+});
+
+function toggleMenu() {
+  if(open == false){
+    menu.style.transform = "translateX(0)";
+    buttonMenu.classList.add("active");
+    open = true;
+    if(window.innerWidth >= 1920){
+      main.style.filter = "blur(5px)";
+    }
+    main.style.display = "none";
+  }else{
+    open = false;
+    main.style.filter = "blur(0px)";
+    main.style.display = "block";
+    menu.style.transform = "translateX(100%)";
+    buttonMenu.classList.remove("active");
+
+  }
+
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
